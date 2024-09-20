f63abf30d4d4106dbbb2f63a47ea0e2f95a6468b
VincentTse <from_vincent@hotmail.com>
2024-09-19 21:34:56 -0400

    changed something in Driver

--- Driver.java
+++ Driver.java
@@ -11,8 +11,8 @@ public class Driver {
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
         if(s.hasRoot(1))
-        System.out.println("1 is a root of s");
+            System.out.println("1 is a root of s");
         else
-    System.out.println("1 is not a root of s");
+            System.out.println("1 is not a root of s");
     }
 }
